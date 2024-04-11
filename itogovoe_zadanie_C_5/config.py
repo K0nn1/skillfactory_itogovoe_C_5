7702e15cc34f4e646ed0e56dce96fee199e55cae
K0nn1 <161319057+K0nn1@users.noreply.github.com>
2024-04-12 02:01:05 +0300

    Add files via upload

---
+++ itogovoe_zadanie_C_5/config.py
@@ -0,0 +1,13 @@
+from TOKEN import TOKEN, API_KEY
+
+
+
+
+currencies = {
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB',
+    'Юань': 'CNY',
+    'Белорусский рубль': 'BYN',
+    'Индийская рупия': 'INR'
+    }
